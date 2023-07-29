decb814acd84339b542bf5f08bccc9c4e49e002b
Eroje <93765911+Eroje@users.noreply.github.com>
2023-07-29 13:24:26 +0100

    3rd Commit

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ class Dog {
   console.log(halley.behavior); // Print behavior value to console
 
 
-  /* Create Instance */
+  /* Create Instances */
 
   /* Create an instance of the Surgeon class.
   Set the name to 'Ruth Jackson' and department to 'Orthopedics'.
@@ -40,5 +40,75 @@ class Dog {
     }
   }
   
-  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics'); 
+    // This creates an instance of Surgeon saved as a constant variable named surgeonJackson                                                              
+
   console.log(surgeonJackson);
+
+  /* Getter and Method and Method Calls */
+
+  class Surgeon { // Class with 3 instances and 2 properties
+    constructor(name, department) { // Constructor with 2 arguments (properties)
+      this._name = name;   // Instances
+      this._department = department; // Instances
+      this._remainingVacationDays = 20; // Instances
+    }
+  
+    get name() { 
+      return this._name;
+    } // Getter 
+  
+    get department() { 
+      return this._department;
+    } // Getter
+  
+    get remainingVacationDays() { 
+      return this._remainingVacationDays;
+    } // Getter
+  
+    takeVacationDays(daysOff) { // Method with 1 argument (daysOff)
+      this._remainingVacationDays -= daysOff; //Subtracts daysOff from remainingVacationDays & save the result to remainingVacationDays
+    } 
+  }
+  
+  const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular'); // Instance 
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics'); // Instance 
+
+  console.log(surgeonRomero.name);
+
+  surgeonRomero.takeVacationDays(3); // Method Calls - (calls takeVacationDays on surgeonRomero)
+  console.log(surgeonRomero.remainingVacationDays);
+
+  /* Inheritance */
+
+  class HospitalEmployee { // Parent Class
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    get name() {
+      return this._name;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  }
+  
+  class Nurse extends HospitalEmployee { // Creates a Nurse class as a Child of the HospitalEmployee class
+    constructor(name, certifications) {
+      super(name); // Calls the parent constructor methods
+      this._certifications = certifications; // Constructs a new instance
+    }
+  }
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
+  nurseOlynyk.takeVacationDays(5); // Method Calls - (calls takeVacationDays on nurseOlynyk)
+  console.log(nurseOlynyk.remainingVacationDays);
+  
+  
+
